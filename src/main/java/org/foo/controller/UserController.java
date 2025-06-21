0c16c9c144a46c7379d8ada51adb3da31551e194
Andi Gela <andi.gela@xponentl.ai>
2025-06-21 23:32:59 +0200

    Take data from generator to service, and back to controller

--- src/main/java/org/foo/controller/UserController.java
+++ src/main/java/org/foo/controller/UserController.java
@@ -1,5 +1,8 @@
 package org.foo.controller;
 import org.foo.dto.UserDTO;
+import org.foo.service.RoleService;
+import org.foo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+    RoleService roleService;
+    UserService userService;
+    @Autowired
+    public UserController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
     @GetMapping("/create")
     public String createUser(Model model) {
         model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", roleService.findAll());
 
         return "/user/create";
     }
