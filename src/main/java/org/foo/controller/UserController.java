e7eb8f3477ff24a04a9cb6ed5e4cf8e81fc22868
AndiGela <andiigela@gmail.com>
2025-06-28 20:34:10 +0200

    Create Update feature

--- src/main/java/org/foo/controller/UserController.java
+++ src/main/java/org/foo/controller/UserController.java
@@ -5,10 +5,7 @@ import org.foo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
@@ -30,9 +27,16 @@ public class UserController {
     }
     @PostMapping("/create")
     public String saveUser(Model model, @ModelAttribute("user") UserDTO userDTO) {
-
         userService.save(userDTO);
         return "redirect:/user/create";
+    }
+    @GetMapping("/update/{username}")
+    public String editUser(@PathVariable("username") String username, Model model){
+
+        model.addAttribute("updateUser", userService.findById(username));
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
+        return "/user/update";
     }
 
 
